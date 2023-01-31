2e66c4c66662d6c0db290d33b59a972ebf42a430
zhouxingfu <xingfu.zhou@zoho.com>
2023-01-31 17:24:14 +0800

    first version can work.

--- src/RouRouThreadPool.h
+++ src/RouRouThreadPool.h
@@ -18,12 +18,55 @@ class RouRouThreadPool
 public:
     RouRouThreadPool(const RouRouThreadPool& pool) = delete;
     RouRouThreadPool(RouRouThreadPool&& pool);
-    RouRouThreadPool(int thread_num);
-    ~RouRouThreadPool();
+    RouRouThreadPool(int thread_num)
+    {
+        for(int i=0; i< thread_num; ++i)
+        {
+            threads_.push_back(std::thread([this](){
+                while(true)
+                {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lck(this->mutex_);
+                        this->cv_.wait(lck, [this](){return this->bStop_ || !tasks_.empty();});//如果运行到此代码时，直接能获取到锁，同时满足条件，那么不需要notify触发，也会往下执行；如果运行到此代码时，无法获取到锁，或者能获取到锁但不满足条件，此时wait阻塞，此时必须被notify触发才能往下执行。notify的作用是触发已阻塞的wait。
+                        if(this->bStop_ && tasks_.empty())
+                            return;
+        
+                        task = std::move(tasks_.front());
+                        tasks_.pop();
+                    }
+                    task();
+                }
+            }));
+        }
+    }
+    ~RouRouThreadPool()
+    {
+        {
+            std::lock_guard<std::mutex> lck(mutex_);
+            bStop_ = true;
+        }
+        cv_.notify_all();
+        for(auto& t : threads_)
+        {
+            t.join();
+        }
+    }
 
 public:
     template<class F, typename... Args>
-    auto enqueue(F&& f, Args&&... args);
+    auto enqueue(F&& f, Args&&... args)
+    {
+        using R = typename std::result_of<F(Args...)>::type;
+        auto task = std::make_shared<std::packaged_task<R()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+        std::future<R> res = task->get_future();
+        {
+            std::lock_guard<std::mutex> lck(mutex_);
+            tasks_.push([task]()->void{(*task)();});
+        }
+        cv_.notify_one();
+        return res;
+    }
 
     std::queue<std::function<void()>>   tasks_;
     std::mutex                          mutex_;
--- src/sample.cxx
+++ src/sample.cxx
@@ -5,7 +5,6 @@
 int main(int argc, char* argv[])
 {
     RouRouThreadPool pool(8);
-    // thread_pool.enqueue([](int a)->int{std::cout << "test" << std::endl; return 100;}, 100);
-    pool.enqueue([](void) {return 100;})
+    pool.enqueue([](void) {std::cout << "hello" << std::endl;});
     return 0;
 }
